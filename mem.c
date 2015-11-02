c961b67c22d9f34f976d8ec0ff0676d630ce2f8b
fwb <844770874@qq.com>
2015-11-02 07:28:44 -0800

    v1.0

---
+++ mem.c
@@ -0,0 +1,178 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include "mem.h"
+#include<sys/mman.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+typedef struct  head_t{
+     int size;
+     void* last;
+     }HEAD;
+
+typedef struct node_t{
+     int size;
+     struct node_t* next;
+     }NODE;
+NODE* n_head;
+int m_error;
+int mem_init(int size_of_region)
+{
+    int fd=open("/dev/zero", O_RDWR);
+    void *ptr=mmap(NULL, size_of_region, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+    if (ptr == MAP_FAILED) { 
+      perror("mmap"); 
+      return -1; 
+      }
+      close(fd);
+      n_head=ptr;
+      n_head->size=size_of_region;
+      n_head->next=NULL;
+      //printf("%d\n",n_head->size);
+      printf("%p\n",ptr);
+    return 0;
+}
+
+void* ops(NODE* rt,int size)
+{
+    NODE* pt,*qt;
+    HEAD* head;
+    int n_size;
+    void* ptr;
+    if(rt==n_head){
+         pt=n_head->next;
+         n_size=n_head->size;
+         head=(HEAD*)n_head;
+         n_head=(NODE*)((char*)n_head+sizeof(HEAD)+size);
+         n_head->size=n_size-(size+sizeof(HEAD));
+         n_head->next=pt;
+         head->size=size;
+         ptr=(void*)head+sizeof(HEAD);
+         head->last=ptr+size;
+         //printf("%p\n%p\n",head,n_head);
+         return ptr;
+         }
+        pt=n_head;
+    while(pt->next!=rt){pt=pt->next;}
+        head=(HEAD*)rt;
+        n_size=rt->size;
+        qt=rt->next;
+        rt=(NODE*)((char*)rt+sizeof(HEAD)+size);
+        rt->size=n_size-(size+sizeof(HEAD));
+        rt->next=qt;
+        pt->next=rt;
+        head->size=size;
+        ptr=(void*)((char*)head+sizeof(HEAD));
+        head->last=ptr+size;
+        return ptr;
+}
+void* mem_alloc(int size,int style)
+{
+   NODE *pt,*qt,*rt;
+   HEAD* head;
+   void* ptr;
+   int t_size,sub;
+   if(size%8!=0){size=(size/8)*8+8;}
+   if(style==M_BESTFIT){//style = best
+      pt=n_head;
+      rt=NULL;
+      t_size=size+sizeof(HEAD);
+      sub=9999999;
+      while(pt!=NULL){
+        if(((pt->size)-t_size>=(int)sizeof(NODE))&&((pt->size)-t_size<sub)){
+          //printf("%d\t%d\t%d\n",sub,t_size,pt->size);
+          sub=(pt->size)-t_size;
+          rt=pt;
+        }
+        pt=pt->next;
+      }
+      if(rt==NULL){
+        m_error=E_NO_SPACE;
+        return NULL;
+       }
+       ptr=ops(rt,size);
+       return ptr;
+      }
+   if(style==M_WORSTFIT){  //style = worst
+   pt=n_head;
+   t_size=0;
+   rt=NULL;
+   while(pt!=NULL){
+       if((pt->size)>t_size){
+          t_size=pt->size;
+          rt=pt;
+          }
+          pt=pt->next;
+          }
+       if(t_size<size+sizeof(HEAD)+sizeof(NODE)){
+        m_error=E_NO_SPACE;
+        return NULL;
+       }
+        ptr=ops(rt,size);
+        return ptr;
+   }
+   if(style==M_FIRSTFIT){// style =first
+   pt=n_head;
+   rt=NULL;
+   t_size=size+sizeof(HEAD);
+   while(pt!=NULL){
+     if((pt->size)-sizeof(NODE)>=t_size){
+          rt=pt;
+          break;
+          }
+      pt=pt->next;
+   }
+   if(rt==NULL){
+        m_error=E_NO_SPACE;
+        return NULL;
+       }
+       ptr=ops(rt,size);
+       return ptr;
+   }
+   m_error=99;                                        //no this style
+   return NULL;
+}
+
+int mem_free(void* ptr)
+{
+  HEAD *pt;
+  NODE *rt,*qt;
+  int n_size;
+  pt=ptr-sizeof(HEAD);
+  rt=n_head;
+  while(rt!=NULL){
+     if((void*)rt==pt->last){}
+     }
+  rt=n_head;
+  while(rt!=NULL){
+     if((void*)rt+rt->size==(void*)pt){}
+     }
+   rt=n_head;
+   n_size=pt->size;
+   qt=(NODE*)pt;
+   while((rt<qt)&&((rt->next)>qt)){rt=rt->next;}
+      qt->size=n_size+sizeof(HEAD);
+      qt->next=rt->next;
+      rt->next=qt; 
+}
+int main()
+{
+  void* ptr;
+  int d=sizeof(HEAD);
+  int i;
+  if(mem_init(10000)==-1){
+  perror("mem_init");
+  exit(1);
+  }
+  for(i=0;i<3;i++){
+  printf("%d\t%d\n",(int)sizeof(HEAD),(int)sizeof(NODE));
+  ptr=mem_alloc(10000,M_WORSTFIT);
+  printf("%p\n",ptr);
+  ptr=mem_alloc(9984,M_BESTFIT);
+  printf("%p\n",ptr);
+  printf("%d\n",n_head->size);
+  ptr=mem_alloc(8,M_FIRSTFIT);
+  printf("%p\n",ptr);
+  }
+  return 0;
+}
